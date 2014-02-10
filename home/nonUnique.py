ada726849920a0b569e2d5daeecb9c28b2001648
makx <mtrofimov@gmail.com>
2014-02-10 19:16:06 +0200

    Add desc

--- home/nonUnique.py
+++ home/nonUnique.py
@@ -1,3 +1,8 @@
+# You are given a list of integers. 
+# For this task, you should return a list consisting of only the non-unique elements in this list. 
+# To do so you will need to remove all unique elements (elements which are contained in a given list only once). 
+# When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3]. 
+
 def nonUnique(data):
     return [element for element in data if data.count(element) > 1]
 
