ae6344e3894464edd181761895b81253349910c5
makx <mtrofimov@gmail.com>
2014-02-11 19:09:18 +0200

    change function name

--- home/password.py
+++ home/password.py
@@ -2,7 +2,7 @@
 # it has at least one number, as well as containing one uppercase letter and one lowercase letter in it.
 
 
-def pass(data):    
+def passw(data):    
     return bool(len(data) >= 10  \
         and filter(lambda x:x.isupper() , data) \
         and filter(lambda x:x.islower(), data) \
@@ -16,12 +16,12 @@ def pass2any(data):
 
 
 if __name__ == '__main__':
-    assert pass('A1213pokl') == False, "1st example"
-    assert pass('bAse730onE4') == True, "2nd example"
-    assert pass('asasasasasasasaas') == False, "3rd example"
-    assert pass('QWERTYqwerty') == False, "4th example"
-    assert pass('123456123456') == False, "5th example"
-    assert pass('QwErTy911poqqqq') == True, "6th example"
+    assert passw('A1213pokl') == False, "1st example"
+    assert passw('bAse730onE4') == True, "2nd example"
+    assert passw('asasasasasasasaas') == False, "3rd example"
+    assert passw('QWERTYqwerty') == False, "4th example"
+    assert passw('123456123456') == False, "5th example"
+    assert passw('QwErTy911poqqqq') == True, "6th example"
     
     assert pass2any('A1213pokl') == False, "1st example"
     assert pass2any('bAse730onE4') == True, "2nd example"
