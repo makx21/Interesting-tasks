546cd610541a9e91f7a169d3b189901838f319ee
makx <mtrofimov@gmail.com>
2014-03-02 12:37:50 +0200

    Add solve transposed matrix

--- home/transMatrix.py
+++ home/transMatrix.py
@@ -1,2 +1,19 @@
 #You have been given a matrix as a 2D list with integers. 
 #Your task is to return a transposed matrix based on input. 
+
+def transMatrix(data):
+    return list(zip(*data))
+    
+if __name__ == '__main__':
+    assert checkio([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]]) == [[1, 4, 7],
+                                    [2, 5, 8],
+                                    [3, 6, 9]], "Square matrix"
+    assert checkio([[1, 4, 3],
+                    [8, 2, 6],
+                    [7, 8, 3],
+                    [4, 9, 6],
+                    [7, 8, 1]]) == [[1, 8, 7, 4, 7],
+                                    [4, 2, 8, 9, 8],
+                                    [3, 6, 3, 6, 1]], "Rectangle matrix"    
