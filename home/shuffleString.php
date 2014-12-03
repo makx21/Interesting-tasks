df5784ea3ab8735e7f26acca6b2ca9e1a3fd174f
makx21 <mtrofimov@gmail.com>
2014-12-03 15:34:29 +0200

    Create shuffleString.php

---
+++ home/shuffleString.php
@@ -0,0 +1,26 @@
+<?php
+
+$string = '{Please|Just} make this {cool|awesome|random} test sentence {rotate {quickly|fast} and random|spin and be random}';
+
+
+function stringShuffle($str){
+
+	$pattern = '/{([^{]+)}/U';
+	$randWords = array();
+
+	preg_match_all($pattern, $str, $matches);
+
+	if (empty($matches[0])){
+		return $str;
+	}
+
+	$randWords = explode('|', substr($matches[0][0], 1, -1));
+
+	shuffle($randWords);
+
+	$shuffleString = str_replace($matches[0][0], $randWords[0], $str);
+
+	return stringShuffle($shuffleString);
+}
+
+echo stringShuffle($string);
